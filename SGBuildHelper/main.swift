42aeb61707148a9d9ea0900160d503e549d36bc2
이세건 <segunlee.partner@iloen.com>
2017-03-29 18:08:08 +0900

    [*] TEST

--- SGBuildHelper/main.swift
+++ SGBuildHelper/main.swift
@@ -7,6 +7,8 @@
 //
 
 import Foundation
+import Cocoa
+
 
 // MARK: -
 enum Mode: String {
@@ -28,44 +30,92 @@ enum Mode: String {
 class Command: NSObject {
     var mode: Mode
     var option: String?
+    var option2: String?
+    
     init(_ modeValue: Mode, _ optionValue: String?) {
         mode = modeValue
         option = optionValue
     }
     
+    init(_ modeValue: Mode, _ optionValue: String?, _ option2Value: String?) {
+        mode = modeValue
+        option = optionValue
+        option2 = option2Value
+    }
+    
     override var description: String {
-        return "(MODE: \(mode) // OPTION: \(option))"
+        return "(MODE: \(mode) // OPTION: \(option) // OPTION2: \(option2))"
     }
 }
 
 
 // MARK: -
-func iconMode() {
+func iconMode(_ filePath: String) {
     print("ICON CHANGE MODE")
+    
+    do {
+        let appIconAppiconsetPath = filePath + "/AppIcon.appiconset"
+        let imageAssetDir = try FileManager.default.contentsOfDirectory(at: URL(fileURLWithPath: appIconAppiconsetPath), includingPropertiesForKeys: nil, options: [])
+        let pngFiles = imageAssetDir.filter({ (URL) -> Bool in
+            URL.pathExtension == "png"
+        })
+        print(pngFiles)
+        // TODO:
+        
+    } catch let error as NSError {
+        print(error.localizedDescription)
+        exit(1)
+    }
 }
 
-func encryptMode(_ filePath: String) {
-    print("ENcRYPT RESOURCES")
+func encryptMode(_ filePath: String, _ descFilePath: String) {
+    print("ENCRYPT START")
+    print("ENCRYPT filePath: \(filePath)")
+    print("ENCRYPT descFilePath: \(descFilePath)")
+    
+    do {
+        let encryptKey = "HELLOWORLD"
+        let encryptFileTypes = ["plist"]
+        let resourceDir = try FileManager.default.contentsOfDirectory(at: URL(fileURLWithPath: filePath), includingPropertiesForKeys: nil, options: [])
+        let plistFiles = resourceDir.filter({ (URL) -> Bool in
+            return encryptFileTypes.contains(URL.pathExtension)
+        })
+        
+        for plistFile in plistFiles {
+            let path = filePath + plistFile.lastPathComponent
+            let descPath = descFilePath + plistFile.lastPathComponent
+            let willEncryptData = try Data(contentsOf: URL(fileURLWithPath: path))
+            let encryptData = try RNEncryptor.encryptData(willEncryptData, with: kRNCryptorAES256Settings, password: encryptKey)
+            try encryptData.write(to: URL(fileURLWithPath: descPath), options: Data.WritingOptions.atomicWrite)
+        }
+        
+    } catch let error as NSError {
+        print(error.localizedDescription)
+        exit(1)
+    }
 }
 
 
 // MARK: -
-let arguments: [String] = CommandLine.arguments//["-i", "-e", "/Root/HomeDirectory/"]
-let commandIsError: Bool = arguments.count & 2 != 0
+let arguments: [String] = CommandLine.arguments
 var commands = [Command]()
 
 for (index, argument) in arguments.enumerated() {
     let mode = Mode(value: argument.substring(from: argument.characters.index(argument.characters.startIndex, offsetBy: 1)))
     switch mode {
     case .ICON:
-        commands.append(Command(mode, nil))
-    case .ENCRYPT:
         print("\(arguments.count) // index is \(index+1)")
         guard arguments.count > index+1 else {
             print("-e option is needed")
             exit(1)
         }
         commands.append(Command(mode, arguments[index+1]))
+    case .ENCRYPT:
+        guard arguments.count > index+2 else {
+            print("-e option, option2 is needed")
+            exit(1)
+        }
+        commands.append(Command(mode, arguments[index+1], arguments[index+2]))
     default: break
     }
 }
@@ -74,9 +124,9 @@ print(commands)
 for command in commands {
     switch command.mode {
     case .ICON:
-        iconMode()
+        iconMode(command.option!)
     case .ENCRYPT:
-        encryptMode(command.option!)
+        encryptMode(command.option!, command.option2!)
     default: break
     }
 }
--- SGRunScript/ViewController.m
+++ SGRunScript/ViewController.m
@@ -7,23 +7,35 @@
 //
 
 #import "ViewController.h"
+#import "RNDecryptor.h"
 
 @interface ViewController ()
-
+@property (nonatomic, strong) NSDictionary *plist;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self loadPlist];
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)loadPlist {
+    NSData *encryptData = [[NSData alloc] initWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"PropertyList" ofType:@"plist"]];
+    NSString *encryptKey = @"HELLOWORLD";
+    NSError *error;
+    NSData *plistData = [RNDecryptor decryptData:encryptData withPassword:encryptKey error:&error];
+    if (plistData) {
+        NSPropertyListFormat propertyListFormat;
+        NSDictionary *plist = [NSPropertyListSerialization propertyListWithData:plistData options:NSPropertyListImmutable format:&propertyListFormat error:&error];
+        if(!plist){
+            NSLog(@"%@", error.localizedDescription);
+        }
+        _plist = plist;
+        NSLog(@"%@", _plist);
+    } else {
+        NSLog(@"plist load error");
+    }
 }
-
 
 @end
